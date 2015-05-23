c9dbe6f18bdacc30d2292869fee873f238482ae6
Eva Müller <em@svenport.de>
2015-05-23 17:18:26 +0200

    Better UI :-)

---
+++ chart.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<title>Peace</title>
+
+<!-- Bootstrap -->
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet"
+	href="libraries/bootstrap/css/bootstrap.3.3.4.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet"
+	href="libraries/bootstrap/css/bootstrap-theme.3.3.4.min.css">
+
+<!-- FONT AWESOME -->
+<link rel="stylesheet"
+	href="libraries/font-awesome-4.3.0/css/font-awesome.min.css">
+
+<!--  PEACH THEME  -->
+<link rel="stylesheet" href="css/moods.css">
+<link rel="stylesheet" href="css/fontsize.css">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+	<div class="container">
+	
+		<div class="row moot_stats_view">
+		
+		</div>
+		
+		<div class="row mood_view_bottom">
+			<div class="col-md-4 mood_view_bottom_icon">
+				<i class="fa fa-cogs fa-2x"></i>
+			</div>
+			<div class="col-md-4 mood_view_bottom_icon">
+				<a href="peace.php"><i class="fa fa-dashboard fa-2x"></i></a>
+			</div>
+			<div class="col-md-4 mood_view_bottom_icon">
+				<a href="chart.php"><i class="fa fa-bar-chart-o fa-2x"></i></a>
+			</div>
+		</div>
+	</div>
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="libraries/jquery-1.11.3.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="libraries/bootstrap/js/bootstrap.3.3.4.min.js"></script>
+
+	<script src="javascript/clickMood.js"></script>
+</body>
+</html>
